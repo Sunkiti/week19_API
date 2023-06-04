5628a6c2db812d2b3239c1f23a80b3e26687d6e7
Sunkiti <elena.bulavkina@gmail.com>
2023-06-04 20:58:41 +0300

    4th commit

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const posts = document.querySelector('posts');
-const btn = document.querySelector('btn');
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then((json) => {
-        json.forEach ((elem) =>{
-            console.log (`${elem.title}`);
-            console.log (`${elem.body}`);
-            const div = document.createElement('div');
-            div.className.add('post__title');
-            div.innerHTML = 
-                `<p>${elem.title}</p>
-                <p>${elem.body}</p>
-                `;
-                posts.appendChild(div); 
-        })
-    }) 
-    .catch((err) => {
-        console.log('Ошибка. Запрос не выполнен');
-    });     
+const posts = document.querySelector('.posts');
+const btn = document.querySelector('.btn');
+function add () {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then((json) => {
+            json.forEach ((elem) =>{
+                console.log (`${elem.title}`);
+                console.log (`${elem.completed}`);
+                const div = document.createElement('div');
+                div.classList.add('post__title');
+                div.innerHTML = 
+                    `<p>${elem.title}</p>
+                    <p>${elem.completed}</p>
+                    `;
+                    posts.appendChild(div); 
+            })
+        });
+        // .catch((err) => {
+        //     console.log('Ошибка. Запрос не выполнен');
+        // }); 
+    }  
+btn.addEventListener ('click', add);
+
+
+
